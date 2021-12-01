143872aa9a97f831f22b341c8dc463b043077619
julioribeiiro <jmoraes@daitan.com>
2021-11-30 23:47:29 -0300

    home page

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,29 +1,39 @@
 import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function Home() {
+    const [userAuthenticate, setUserAuthenticate] = useState(false)
     const [userRole, setUserRole] = useState(0)
 
+    let navigate = useNavigate();
+
     useEffect(() => {
+        setUserAuthenticate(localStorage.getItem('token'))
         // setUserRole(localStorage.getItem('role'));
     }, [])
 
     return (
-        <div style={{
-            width: '100%', 
-            height: '100%', 
-            display: 'flex', 
-            justifyContent: 'center', 
-            alignItems: 'center', 
-            flexDirection: 'column'}}>
-            <h1>Normal user content</h1>
-            {
-                userRole === 1 && (
-                    <h1>
-                        Admin user content
-                    </h1>
-                )
-            }
-        </div>
+        userAuthenticate ? 
+            (<div style={{
+                width: '100%', 
+                height: '100%', 
+                display: 'flex', 
+                justifyContent: 'center', 
+                alignItems: 'center', 
+                flexDirection: 'column'}}>
+                <h1>Normal user content</h1>
+                {
+                    userRole === 1 && (
+                        <h1>
+                            Admin user content
+                        </h1>
+                    )
+                }
+            </div>) :
+            (<div>
+                <h1>Please authenticate before access this page</h1>
+                <button onClick={() => {navigate('/')}}>Login page</button>
+            </div>)    
     );
 }
 
