17a9230657c860848a4cce9b16738732838e906d
DavidInojosaNeurobots <49165221+DavidInojosa@users.noreply.github.com>
2021-11-30 23:13:21 -0300

    atualizacao

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -3,14 +3,18 @@ import React from 'react';
 // import { Container } from './styles';
 
 function LoginPage() {
+    const [name, setName] = useState("");
+    const [password, setPassword] = useState("");
+
   return (
     <div>
         <h1>Login Page</h1>
-        <form>
+        <form >
             <label>login</label>
-            <input type='text' name='login' />
+            <input type='text' name='login' value={name} onChange={(e) => setName(e.target.value)}/>
             <label>password</label>
-            <input type='password' name='pass' />
+            <input type='password' name='pass' value={password} onChange={(e) => setPassword(e.target.value)}/>
+            <input type="submit" value="Submit"/>
         </form>
     </div>);
 }
